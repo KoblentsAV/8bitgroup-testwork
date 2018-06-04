ec2b74699b06d794b42dc3a2e07159edb2338571
Alexander Koblents <koblents1945@gmail.com>
2018-06-04 13:53:51 +0300

    Завершает тестовое задание

---
+++ js/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+$(document).ready(function(){
+  $("a[href*=#]").on("click", function(e){
+      var anchor = $(this);
+      $('html, body').stop().animate({
+          scrollTop: $(anchor.attr('href')).offset().top
+      }, 2000);
+      e.preventDefault();
+      return false;
+  });
+});
